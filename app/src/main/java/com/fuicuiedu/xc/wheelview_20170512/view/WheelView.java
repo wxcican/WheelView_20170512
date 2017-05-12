2fa646c7acc7bc5d7ebadfc7b0c10c0ed2716307
wxcican <wxcican@qq.com>
2017-05-12 11:01:32 +0800

    手指滑动时实时改变选中的条目

--- app/src/main/java/com/fuicuiedu/xc/wheelview_20170512/view/WheelView.java
+++ app/src/main/java/com/fuicuiedu/xc/wheelview_20170512/view/WheelView.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 
 import com.fuicuiedu.xc.wheelview_20170512.R;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 /**
@@ -118,9 +120,101 @@ public class WheelView extends LinearLayout{
     //处理触摸事件
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // TODO: 2017/5/12 0012 摁下，滑动，松开 
-        return super.onTouchEvent(event);
+        //条目为空时不做处理
+        if (wheelItems == null || wheelItems.size() <= 0){
+            return true;
+        }
+
+        //添加一个用户的运动跟踪器
+        tracker.addMovement(event);
+        //获取到初始点的Y轴坐标
+        int y = (int) event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                //摁下不错处理
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dexY = y - lastY;
+                //手指滑动时使用，实时改变选中的条目
+                actionMove(getScrollY(),dexY);
+                break;
+            case MotionEvent.ACTION_UP:
+                //手指松开后继续滑动一段距离
+                actionUp(getScrollY());
+                //todo 选择后提示用户 -------------- 接口回调
+                break;
+        }
+
+        lastY = y;
+        return true;
     }
+
+    // ##################################    逻辑处理  ########################
+    //手指滑动时使用，实时改变选中的条目
+    private void actionMove(int scrollY,int dexY){
+        //根据手指的滑动速度获取到当前选中的条目
+        getSelectdIndex(scrollY);
+        scrollBy(0,-dexY);//向上滑动控件
+    }
+
+    //根据手指的滑动速度获取到当前选中的条目
+    private void getSelectdIndex(int scrollY){
+
+        tracker.computeCurrentVelocity(100);//设置时间单位为0.1秒
+        float yVelocity = tracker.getYVelocity();//Y轴移动了多少像素
+
+        //如果移动像素绝对值大于一个条目高度
+        if (Math.abs(yVelocity) >= unitHeight){
+            //像素是大于0则向上滑动，否则向下
+            int count = (int) (yVelocity / changeUnit(TypedValue.COMPLEX_UNIT_PX,unitHeight) / 5);
+            currentIndex = currentIndex - count;
+        } else{
+            if (scrollY > 0){
+                //scrollY 大于0 表向itemCount/2 的下方项移动，否则是向上方项移动
+                currentIndex = (scrollY + unitHeight/2)/unitHeight + firstSelectedIndex;
+            }else {
+                currentIndex = (scrollY - unitHeight/2)/unitHeight + firstSelectedIndex;
+            }
+        }
+
+        //限制只能为0，或条目的最大数目
+        currentIndex = Math.max(0,Math.min(currentIndex,wheelItems.size()-1));
+        //设置选中的文本字体，其他未选中的使用默认的字体
+        setSelectedFont();
+        tracker.clear();
+    }
+
+    //设置选中的文本字体，其他未选中的使用默认的字体
+    private void setSelectedFont(){
+        for (int i = 0; i < wheelItems.size(); i++) {
+            TextView tv = wheelItems.get(i);
+            tv.setTextSize(normalFontSize);
+            tv.setTextColor(normalFontColor);
+
+            int j = Math.abs(i - currentIndex);
+            //选中项两边的字体逐渐变小
+            if ((normalFontSize - j*2) > 0) tv.setTextSize(normalFontSize - 2*j);
+
+            if (i == currentIndex){
+                tv.setTextSize(selectedFontSize);
+                tv.setTextColor(selectedFontColor);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //将传进来的值变为期望的值
     //unit 期望转换得到值的类型（TypedValue.COMPLEX_UNIT_DIP,得到dp类型的值）
